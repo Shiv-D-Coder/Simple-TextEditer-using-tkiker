5914d67489f18fa38b73db44515c24932de333e0
Shiv-D-Coder <shivtheworld@gmail.com>
2023-08-01 22:13:11 +0530

    adding commrnts

--- notpad.py
+++ notpad.py
@@ -6,31 +6,35 @@ from tkinter.filedialog import *
 
 class Notepad:
 
+	# Initialize the tkinter root window
 	__root = Tk()
 
-	# default window width and height
+	# Default window width and height
 	__thisWidth = 300
 	__thisHeight = 300
+
+	# Text area to display and edit the text
 	__thisTextArea = Text(__root)
+
+	# Menu bar and its menus
 	__thisMenuBar = Menu(__root)
 	__thisFileMenu = Menu(__thisMenuBar, tearoff=0)
 	__thisEditMenu = Menu(__thisMenuBar, tearoff=0)
 	__thisHelpMenu = Menu(__thisMenuBar, tearoff=0)
 	
-	# To add scrollbar
+	# Scrollbar for the text area
 	__thisScrollBar = Scrollbar(__thisTextArea)	
 	__file = None
 
-	def __init__(self,**kwargs):
+	def __init__(self, **kwargs):
 
-		# Set icon
+		# Set icon for the Notepad window
 		try:
-				self.__root.wm_iconbitmap("Notepad.ico")
+			self.__root.wm_iconbitmap("Notepad.ico")
 		except:
-				pass
+			pass
 
 		# Set window size (the default is 300x300)
-
 		try:
 			self.__thisWidth = kwargs['width']
 		except KeyError:
@@ -41,159 +45,118 @@ class Notepad:
 		except KeyError:
 			pass
 
-		# Set the window text
+		# Set the window title
 		self.__root.title("Untitled - Notepad")
 
-		# Center the window
+		# Center the window on the screen
 		screenWidth = self.__root.winfo_screenwidth()
 		screenHeight = self.__root.winfo_screenheight()
-	
-		# For left-align
 		left = (screenWidth / 2) - (self.__thisWidth / 2)
-		
-		# For right-align
-		top = (screenHeight / 2) - (self.__thisHeight /2)
-		
-		# For top and bottom
+		top = (screenHeight / 2) - (self.__thisHeight / 2)
 		self.__root.geometry('%dx%d+%d+%d' % (self.__thisWidth,
 											self.__thisHeight,
 											left, top))
 
-		# To make the textarea auto resizable
+		# Make the textarea auto resizable
 		self.__root.grid_rowconfigure(0, weight=1)
 		self.__root.grid_columnconfigure(0, weight=1)
 
-		# Add controls (widget)
-		self.__thisTextArea.grid(sticky = N + E + S + W)
-		
-		# To open new file
-		self.__thisFileMenu.add_command(label="New",
-										command=self.__newFile)
-		
-		# To open a already existing file
-		self.__thisFileMenu.add_command(label="Open",
-										command=self.__openFile)
-		
-		# To save current file
-		self.__thisFileMenu.add_command(label="Save",
-										command=self.__saveFile)
+		# Add controls (widgets) to the window
+		self.__thisTextArea.grid(sticky=N + E + S + W)
 
-		# To create a line in the dialog	
-		self.__thisFileMenu.add_separator()										
-		self.__thisFileMenu.add_command(label="Exit",
-										command=self.__quitApplication)
-		self.__thisMenuBar.add_cascade(label="File",
-									menu=self.__thisFileMenu)	
-		
-		# To give a feature of cut
-		self.__thisEditMenu.add_command(label="Cut",
-										command=self.__cut)			
-	
-		# to give a feature of copy
-		self.__thisEditMenu.add_command(label="Copy",
-										command=self.__copy)		
-		
-		# To give a feature of paste
-		self.__thisEditMenu.add_command(label="Paste",
-										command=self.__paste)		
-		
-		# To give a feature of editing
-		self.__thisMenuBar.add_cascade(label="Edit",
-									menu=self.__thisEditMenu)	
-		
-		# To create a feature of description of the notepad
-		self.__thisHelpMenu.add_command(label="About Notepad",
-										command=self.__showAbout)
-		self.__thisMenuBar.add_cascade(label="Help",
-									menu=self.__thisHelpMenu)
+		# Menu options for File menu
+		self.__thisFileMenu.add_command(label="New", command=self.__newFile)
+		self.__thisFileMenu.add_command(label="Open", command=self.__openFile)
+		self.__thisFileMenu.add_command(label="Save", command=self.__saveFile)
+		self.__thisFileMenu.add_separator()
+		self.__thisFileMenu.add_command(label="Exit", command=self.__quitApplication)
+		self.__thisMenuBar.add_cascade(label="File", menu=self.__thisFileMenu)	
 
+		# Menu options for Edit menu
+		self.__thisEditMenu.add_command(label="Cut", command=self.__cut)
+		self.__thisEditMenu.add_command(label="Copy", command=self.__copy)
+		self.__thisEditMenu.add_command(label="Paste", command=self.__paste)
+		self.__thisMenuBar.add_cascade(label="Edit", menu=self.__thisEditMenu)	
+
+		# Menu options for Help menu
+		self.__thisHelpMenu.add_command(label="About Notepad", command=self.__showAbout)
+		self.__thisMenuBar.add_cascade(label="Help", menu=self.__thisHelpMenu)
+
+		# Attach the menu bar to the root window
 		self.__root.config(menu=self.__thisMenuBar)
 
-		self.__thisScrollBar.pack(side=RIGHT,fill=Y)				
-		
-		# Scrollbar will adjust automatically according to the content	
-		self.__thisScrollBar.config(command=self.__thisTextArea.yview)	
+		# Attach a scrollbar to the text area
+		self.__thisScrollBar.pack(side=RIGHT, fill=Y)				
+		self.__thisScrollBar.config(command=self.__thisTextArea.yview)
 		self.__thisTextArea.config(yscrollcommand=self.__thisScrollBar.set)
 	
 		
 	def __quitApplication(self):
+		# Destroy the root window and exit the application
 		self.__root.destroy()
-		# exit()
 
 	def __showAbout(self):
-		showinfo("Notepad","Mrinal Verma")
+		# Display information about the Notepad (About dialog)
+		showinfo("Notepad", "Mrinal Verma")
 
 	def __openFile(self):
-		
+		# Open an existing file and display its content in the text area
 		self.__file = askopenfilename(defaultextension=".txt",
-									filetypes=[("All Files","*.*"),
-										("Text Documents","*.txt")])
+									filetypes=[("All Files", "*.*"),
+												("Text Documents", "*.txt")])
 
 		if self.__file == "":
-			
-			# no file to open
+			# No file selected or canceled
 			self.__file = None
 		else:
-			
-			# Try to open the file
-			# set the window title
+			# Try to open the file and display its content
 			self.__root.title(os.path.basename(self.__file) + " - Notepad")
-			self.__thisTextArea.delete(1.0,END)
+			self.__thisTextArea.delete(1.0, END)
+			with open(self.__file, "r") as file:
+				self.__thisTextArea.insert(1.0, file.read())
 
-			file = open(self.__file,"r")
-
-			self.__thisTextArea.insert(1.0,file.read())
-
-			file.close()
-
-		
 	def __newFile(self):
+		# Create a new blank file in the text editor
 		self.__root.title("Untitled - Notepad")
 		self.__file = None
-		self.__thisTextArea.delete(1.0,END)
+		self.__thisTextArea.delete(1.0, END)
 
 	def __saveFile(self):
-
-		if self.__file == None:
-			# Save as new file
+		if self.__file is None:
+			# Save as a new file
 			self.__file = asksaveasfilename(initialfile='Untitled.txt',
 											defaultextension=".txt",
-											filetypes=[("All Files","*.*"),
-												("Text Documents","*.txt")])
+											filetypes=[("All Files", "*.*"),
+														("Text Documents", "*.txt")])
 
 			if self.__file == "":
 				self.__file = None
 			else:
-				
 				# Try to save the file
-				file = open(self.__file,"w")
-				file.write(self.__thisTextArea.get(1.0,END))
-				file.close()
-				
-				# Change the window title
+				with open(self.__file, "w") as file:
+					file.write(self.__thisTextArea.get(1.0, END))
 				self.__root.title(os.path.basename(self.__file) + " - Notepad")
-				
-			
 		else:
-			file = open(self.__file,"w")
-			file.write(self.__thisTextArea.get(1.0,END))
-			file.close()
+			# Save the existing file
+			with open(self.__file, "w") as file:
+				file.write(self.__thisTextArea.get(1.0, END))
 
 	def __cut(self):
+		# Cut selected text in the text area
 		self.__thisTextArea.event_generate("<<Cut>>")
 
 	def __copy(self):
+		# Copy selected text in the text area
 		self.__thisTextArea.event_generate("<<Copy>>")
 
 	def __paste(self):
+		# Paste text from the clipboard into the text area
 		self.__thisTextArea.event_generate("<<Paste>>")
 
 	def run(self):
-
-		# Run main application
+		# Run the main application
 		self.__root.mainloop()
 
-
 # Run main application
-notepad = Notepad(width=600,height=400)
+notepad = Notepad(width=600, height=400)
 notepad.run()
